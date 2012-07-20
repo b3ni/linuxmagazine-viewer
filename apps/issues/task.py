f1f6bfc23cac3c1335c1bcddbb57b9661df9b02b
b3ni <brarcos@gmail.com>
2012-07-20 11:48:11 +0200

    add folders

---
+++ apps/issues/task.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from utils.tasks import BasePeriodicTask
+
+
+class ReadIssuesPeriodicTask(BasePeriodicTask):
+    """
+    Comprueba si hay ediciones nuevas de la revista para ser descargadas
+    """
+    name = "ReadIssuesPeriodicTask"
+    run_every = config_lmv.LMV_TASK_CHECK_ISSUES
+
+    def _do_task(self, *args, **kwargs):
+        r = requests.get(config_lmv.LMV_URL_ISSUES)
+        if r.status_code != 200:
+            self.logger.error(u"No se ha podido leer '%s'" % config_lmv.LMV_URL_ISSUES)
+
+        dom = xmlDoc_from_html(r.text)
+        for link in enumerate(dom.xpath('//a[contains(@href, "/issue/")]')):
+            print link
+
+
+    # def parse(self, response):
+    #     dom = HtmlXPathSelector(response)
+
+    #     numeros = []
+    #     for index, link in enumerate(dom.select('//a[contains(@href, "/issue/")]')):
+    #         href = link.select('@href').extract()[0]
+    #         text = link.extract()
+
+    #         item = LinkMagazineItem()
+    #         item['number'] = int(href.split('/')[2])
+    #         item['title'] = text
+    #         item['link'] = 'http://www.linux-magazine.es' + href
+
+    #         numeros.append(item)
+
+    #     return numeros
---
+++ utils/tasks.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from config_celery import acquire_lock, release_lock
+from celery.task.base import PeriodicTask
+
+
+class BasePeriodicTask(PeriodicTask):
+    """ Base para tareas periodicas """
+    logger = None
+
+    def run(self, *args, **kwargs):
+        self.logger = self.get_logger(**kwargs)
+
+        name_log = self.__class__.__name__.upper()
+        name_lock = self.__class__.__name__.lower()
+
+        self.logger.info(u"TASK: " + name_log)
+
+        # LOCK
+        lock_id = '%s-lock' % (name_lock)
+        if not acquire_lock(lock_id):
+            self.logger.info(u'%s (%s) running' % (name_log, lock_id))
+            return
+
+        # TASK
+        try:
+            self._do_task(*args, **kwargs)
+        finally:
+            release_lock(lock_id)
+
+        self.logger.info(u"END: TASK " + name_log)
+
+    def _do_task(self, *args, **kwargs):
+        raise NotImplementedError()
